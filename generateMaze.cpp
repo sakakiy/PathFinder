3e77467b91f6cd4a4cc7fdfe4c2014258dd2bae6
榊原吉伸 <sakura.colonnade@gmail.com>
2014-07-11 00:11:24 +0900

    迷路生成（generateMaze）と迷路表示（printMaze）を関数化。

--- generateMaze.cpp
+++ generateMaze.cpp
@@ -4,11 +4,19 @@ using namespace std;
 
 const int width  = 15;
 const int height = 20;
-char buffer[height][width + 2];
 
+
+void generateMaze(char buf[][width + 2]);
 void saveMazeFile(char buf[][width + 2], const char* name);
+void printMaze(char buf[][width + 2]);
 
+////
+// main
+//
 int main(int argc, char* argv[]){
+  
+  // 迷路バッファ
+  char buffer[height][width + 2];
   
   // バッファの初期化
   for(int i=0; i<height; i++){
@@ -21,29 +29,53 @@ int main(int argc, char* argv[]){
     buffer[i][width + 1] = '\0';
   }
   
-  // 確認、改行コードを含んでいるので表示の際に開業しない
+  // 迷路を生成する
+  generateMaze(buffer);
+
+  // 迷路を表示する
+  printMaze(buffer);
+
+  // 迷路をファイルに保存
+  saveMazeFile(buffer, "maze.txt");
+}
+
+////
+// 迷路バッファを表示する
+//
+void printMaze(char buf[][width + 2]){
+  
+  // 確認、改行コードを含んでいるので表示のときに改行しない
   for(int i=0; i<height; i++){
-    printf("%3d : %s", i, buffer[i]);
+    printf("%3d : %s", i, buf[i]);
   }
+  
+}
+
+////
+// 迷路を生成する
+//
+void generateMaze(char buf[][width + 2]){ 
   
   // ランダムで何か入れる（予定）
   for(int i=0; i<height; i++){
     for(int j=0; j<width; j++){
-      buffer[i][j] = '0';
+      buf[i][j] = '0';
     }
   }
   
-  saveMazeFile(buffer, "maze.txt");
 }
 
+////
 // 迷路をファイルに書き込む
+//
 void saveMazeFile(char buf[][width + 2], const char* name){
+  
   // ファイルポインタ
   FILE* pFile = fopen(name, "w");
   
   // ファイル書き込み
   for(int i=0; i<height; i++){
-    fputs(buffer[i], pFile);
+    fputs(buf[i], pFile);
   }
   fclose(pFile);
   
