454bb610dce443a858248c2622718c58f282471b
榊原吉伸 <sakura.colonnade@gmail.com>
2014-07-13 23:02:15 +0900

    width , height を WIDTH , HEIGHT に変更

--- generateMaze.cpp
+++ generateMaze.cpp
@@ -4,13 +4,13 @@
 
 using namespace std;
 
-const int width  = 15;
-const int height = 20;
+const int WIDTH  = 15;
+const int HEIGHT = 20;
 
 
-void generateMaze(char buf[][width + 2]);
-void saveMazeFile(char buf[][width + 2], const char* name);
-void printMaze(char buf[][width + 2]);
+void generateMaze(char buf[][WIDTH + 2]);
+void saveMazeFile(char buf[][WIDTH + 2], const char* name);
+void printMaze(char buf[][WIDTH + 2]);
 
 ////
 // main
@@ -18,17 +18,17 @@ void printMaze(char buf[][width + 2]);
 int main(int argc, char* argv[]){
   
   // 迷路バッファ
-  char buffer[height][width + 2];
+  char buffer[HEIGHT][WIDTH + 2];
   
   // バッファの初期化
-  for(int i=0; i<height; i++){
-    for(int j=0; j<width; j++){
+  for(int i=0; i<HEIGHT; i++){
+    for(int j=0; j<WIDTH; j++){
       buffer[i][j] = '0';
     }
     
     // ヌル文字と改行を文末につける
-    buffer[i][width] = '\n';
-    buffer[i][width + 1] = '\0';
+    buffer[i][WIDTH] = '\n';
+    buffer[i][WIDTH + 1] = '\0';
   }
   
   // 迷路を生成する
@@ -44,10 +44,10 @@ int main(int argc, char* argv[]){
 ////
 // 迷路バッファを表示する
 //
-void printMaze(char buf[][width + 2]){
+void printMaze(char buf[][WIDTH + 2]){
   
   // 確認、改行コードを含んでいるので表示のときに改行しない
-  for(int i=0; i<height; i++){
+  for(int i=0; i<HEIGHT; i++){
     printf("%3d : %s", i, buf[i]);
   }
   
@@ -56,7 +56,7 @@ void printMaze(char buf[][width + 2]){
 ////
 // 迷路を生成する
 //
-void generateMaze(char buf[][width + 2]){
+void generateMaze(char buf[][WIDTH + 2]){
   
   // 乱数初期化
   srand((unsigned)time(NULL));
@@ -64,8 +64,8 @@ void generateMaze(char buf[][width + 2]){
   int randomNum;
   
   // ランダムで何か入れる（予定）
-  for(int i=1; i<height - 1; i++){
-    for(int j=1; j<width - 1; j++){
+  for(int i=1; i<HEIGHT - 1; i++){
+    for(int j=1; j<WIDTH - 1; j++){
       // buf[i][j] = '0';
       //itoa(rand() % 5, &ascii, 10);
       
