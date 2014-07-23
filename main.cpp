8e03fc905e2777b20d5fa9719d28e555c786cc06
榊原吉伸 <sakura.colonnade@gmail.com>
2014-07-23 23:31:15 +0900

    単純なスタック作成　次はクラス化か汎用型化

--- main.cpp
+++ main.cpp
@@ -1,7 +1,61 @@
 #include <iostream>
+#include <unistd.h>
+#include <time.h>
+
+using namespace std;
+
+int ind = 0;
+const int MAX = 10;
+int array[MAX];
+
+void push(int);
+int  pop();
+void printArray();
 
 int main(){
-
+  
+  srand((unsigned)time(NULL));
+  for(int i=0; i<MAX; i++){
+    array[i] = 0;
+  }
+  
+  for(int i=0; i<20; i++){
+    if(rand()%2==0){
+      push(rand()%10);
+      printArray();
+    } else {
+      pop();
+      printArray();      
+    }
+  }
 
   return 0;
+}
+
+void push(int a){
+  if(ind < MAX){
+    array[ind] = a;
+    ind++;
+  } else {
+    cout << "stack full.\n";
+  }
+}
+
+int pop(){
+  if(0 < ind){
+    ind--;
+    return array[ind];
+  } else {
+    cout << "stack empty.\n";
+    return -1;
+  }
+
+}
+
+void printArray(){
+  cout << "array : ";
+  for(int i=0; i<ind; i++){
+    cout << array[i] << " ";
+  }
+  cout << "\n";
 }
